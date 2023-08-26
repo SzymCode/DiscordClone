329584b6bec3d50a72e29cf852a9a4ba61a1ced5
SzymCode <s.radomski19@gmail.com>
2023-08-26 22:08:20 +0200

    Chat functionality

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -2,14 +2,51 @@ import { HashtagIcon, SearchIcon } from "@heroicons/react/outline";
 import { BellIcon, ChatIcon, UsersIcon, InboxIcon, QuestionMarkCircleIcon, PlusCircleIcon, GiftIcon, EmojiHappyIcon } from "@heroicons/react/solid";
 import { useSelector } from "react-redux";
 import { selectChannelId, selectChannelName } from "../features/channelSlice";
+import { auth, db } from "../firebase";
+import Message from "./Message";
+import firebase from "firebase";
 import {  useRef } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
 
-function Chat() {
+export default function Chat() {
   const channelId = useSelector(selectChannelId);
   const channelName = useSelector(selectChannelName);
+  const [user] = useAuthState(auth);
   const inputRef = useRef("");
   const chatRef = useRef(null);
+  const [messages] = useCollection(
+    channelId &&
+      db
+        .collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+  );
 
+  function scrollToBottom() {
+    chatRef.current.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  };
+
+  function sendMessage(e) {
+    e.preventDefault();
+
+    if (inputRef.current.value !== "") {
+      db.collection("channels").doc(channelId).collection("messages").add({
+        timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        message: inputRef.current.value,
+        name: user?.displayName,
+        photoURL: user?.photoURL,
+        email: user?.email,
+      });
+    }
+
+    inputRef.current.value = "";
+    scrollToBottom();
+  };
 
   return (
     <div className="flex flex-col h-screen">
@@ -23,7 +60,11 @@ function Chat() {
           <ChatIcon className="icon" />
           <UsersIcon className="icon" />
           <div className="flex bg-[#202225] text-xs p-1 rounded-md">
-            <input type="text" placeholder="Search" className="bg-[#202225] focus:outline-none text-white pl-1 placeholder-[#72767d]" />
+            <input
+              type="text"
+              placeholder="Search"
+              className="bg-[#202225] focus:outline-none text-white pl-1 placeholder-[#72767d]"
+            />
             <SearchIcon className="h-4 text-[#72767d] mr-1" />
           </div>
           <InboxIcon className="icon" />
@@ -31,14 +72,36 @@ function Chat() {
         </div>
       </header>
       <main className="flex-grow overflow-y-scroll scrollbar-hide">
+        {messages?.docs.map((doc) => {
+          const { message, timestamp, name, photoURL, email } = doc.data();
+
+          return (
+            <Message
+              key={doc.id}
+              id={doc.id}
+              message={message}
+              timestamp={timestamp}
+              name={name}
+              email={email}
+              photoURL={photoURL}
+            />
+          );
+        })}
         <div ref={chatRef} className="pb-16" />
       </main>
       <div className="flex items-center p-2.5 bg-[#40444b] mx-5 mb-7 rounded-lg">
         <PlusCircleIcon className="icon mr-4" />
         <form className="flex-grow">
-          <input type="text" disabled={!channelId} placeholder={channelId ? `Message #${channelName}` : "Select a channel"}
-              className="bg-transparent focus:outline-none text-[#dcddde] w-full placeholder-[#72767d] text-sm" ref={inputRef} />
-          <button hidden type="submit">
+          <input
+            type="text"
+            disabled={!channelId}
+            placeholder={
+              channelId ? `Message #${channelName}` : "Select a channel"
+            }
+            className="bg-transparent focus:outline-none text-[#dcddde] w-full placeholder-[#72767d] text-sm"
+            ref={inputRef}
+          />
+          <button hidden type="submit" onClick={sendMessage}>
             Send
           </button>
         </form>
@@ -48,5 +111,3 @@ function Chat() {
     </div>
   );
 }
-
-export default Chat;
